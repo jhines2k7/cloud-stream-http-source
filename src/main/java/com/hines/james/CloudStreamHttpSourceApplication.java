c1f4f2f697463b1c9170304dd7a5d7d34647b10d
James Hines <jameshines10@gmail.com>
2017-04-12 11:12:20 -0400

    Initial commit

---
+++ src/main/java/com/hines/james/CloudStreamHttpSourceApplication.java
@@ -0,0 +1,12 @@
+package com.hines.james;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamHttpSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStreamHttpSourceApplication.class, args);
+	}
+}
