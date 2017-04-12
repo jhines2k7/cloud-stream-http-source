78e4c24459a2efa4f67e8cd058266a367240fbe6
James Hines <jameshines10@gmail.com>
2017-04-12 13:29:54 -0400

    Slight refactor to simplify the application

--- src/main/java/com/hines/james/CloudStreamHttpSourceApplication.java
+++ src/main/java/com/hines/james/CloudStreamHttpSourceApplication.java
@@ -22,24 +22,20 @@ import java.util.Collections;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @SpringBootApplication
+@Controller
+@EnableBinding(Source.class)
 public class CloudStreamHttpSourceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStreamHttpSourceApplication.class, args);
 	}
-}
 
-@Configuration
-@Controller
-@EnableBinding(Source.class)
-class HttpSourceConfiguration {
+    @Autowired
+    private Source channels;
 
-	@Autowired
-	private Source channels;
-
-	@RequestMapping(path = "${http.pathPattern:/}", method = POST, consumes = {"text/*", "application/json"})
-	@ResponseStatus(HttpStatus.ACCEPTED)
-	public void handleRequest(@RequestBody String body, @RequestHeader(HttpHeaders.CONTENT_TYPE) Object contentType) throws MyCustomException {
+    @RequestMapping(path = "${http.pathPattern:/}", method = POST, consumes = {"text/*", "application/json"})
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void handleRequest(@RequestBody String body, @RequestHeader(HttpHeaders.CONTENT_TYPE) Object contentType) throws MyCustomException {
         JSONObject obj = new JSONObject(body);
 
         if(obj.getString("foo").equals("bar")) {
@@ -48,18 +44,18 @@ class HttpSourceConfiguration {
             System.out.println(obj.getString("foo"));
             sendMessage(body, contentType);
         }
-	}
+    }
 
-	@RequestMapping(path = "${http.pathPattern:/}", method = POST, consumes = "*/*")
-	@ResponseStatus(HttpStatus.ACCEPTED)
-	public void handleRequest(@RequestBody byte[] body, @RequestHeader(HttpHeaders.CONTENT_TYPE) Object contentType) {
-		sendMessage(body, contentType);
-	}
+    @RequestMapping(path = "${http.pathPattern:/}", method = POST, consumes = "*/*")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void handleRequest(@RequestBody byte[] body, @RequestHeader(HttpHeaders.CONTENT_TYPE) Object contentType) {
+        sendMessage(body, contentType);
+    }
 
-	private void sendMessage(Object body, Object contentType) {
-		channels.output().send(MessageBuilder.createMessage(body,
-				new MessageHeaders(Collections.singletonMap(MessageHeaders.CONTENT_TYPE, contentType))));
-	}
+    private void sendMessage(Object body, Object contentType) {
+        channels.output().send(MessageBuilder.createMessage(body,
+                new MessageHeaders(Collections.singletonMap(MessageHeaders.CONTENT_TYPE, contentType))));
+    }
 }
 
 @ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Foo failed validation")
